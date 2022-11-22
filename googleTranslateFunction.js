45cc81653e4354fe378d5276d1a010d91a96c1af
atiurrahman-cedcoss <atiurrahmanansari@cedcommerce.com>
2022-11-22 16:01:12 +0530

    update

--- googleTranslateFunction.js
+++ googleTranslateFunction.js
@@ -1,15 +1,16 @@
 function googleTranslateElementInit() {
-    new google.translate.TranslateElement(
-      { pageLanguage: 'en' },
-      'google_translate_element'
-    );
-    // Create element:
-const para = document.createElement("div");
-para.className = "google_translate_element";
+  new google.translate.TranslateElement(
+    { pageLanguage: 'en' },
+    'google_translate_element'
+  );
+  // Create element:
+  const para = document.createElement("div");
+  para.className = "google_translate_element";
 
-// Append to another element:
-document.getElementsByTagName("body").appendChild(para);
+  // Append to another element:
+  // document.getElementsByTagName("body").appendChild(para);
+  document.body.appendChild(para);
 
-  }
+}
 
 googleTranslateElementInit()
