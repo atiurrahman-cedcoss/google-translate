4d6bccb4fb76fe1fec9a26864f36487fb4a6cce6
atiurrahman-cedcoss <98949283+atiurrahman-cedcoss@users.noreply.github.com>
2022-12-15 00:29:20 +0530

    update3

--- googleTranslateFunction.js
+++ googleTranslateFunction.js
@@ -1,15 +1,17 @@
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement(
-    { pageLanguage: 'en' },
-    'google_translate_element'
-  );
-  // Create element:
-  const para = document.createElement("div");
-  para.id = "google_translate_element";
+// function googleTranslateElementInit() {
+//   new google.translate.TranslateElement(
+//     { pageLanguage: 'en' },
+//     'google_translate_element'
+//   );
+//   // Create element:
+//   const para = document.createElement("div");
+//   para.id = "google_translate_element";
 
-  // Append to another element:
-  document.body.appendChild(para);
-  console.log("window",window)
-}
+//   // Append to another element:
+//   document.body.appendChild(para);
+//   console.log("window",window)
+// }
 
-googleTranslateElementInit()
+// googleTranslateElementInit()
+
+console.log("window",window)
